82a52172d09c591b5f45f5449fdacb21a1c85ee7
Edd <pough.edward2003@gmail.com>
2026-01-21 11:26:01 +0100

    Add files via upload

--- Druemmer/Druemmer BackUp/DruemmerAbilities.py
+++ Druemmer/Druemmer BackUp/DruemmerAbilities.py
@@ -39,66 +39,61 @@ class abilities():
         self.faiscl = "" #Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2%
         self.function = None #Function of said Ability
 
-    def overheadsmash(self, hp, mp, dmg, str, dex, arc, fai, hit, crit):
+#---------------MACE---------------
+    def overheadsmash(self, hp, mp, dmg, str, dex, arc, fai, hit, crit, run):
+        self.name = "" #For Display in Combat only
+        self.hpcost = 0 #For Display in Combat only | Custom Value
+        self.mpcost = 3 #For Display in Combat only | Custom Value
+        self.finaldmg = 0
+        self.finalheal = 0
+        self.hitchance = hit + 0 #For Display in Combat only | Weaponhitchance + Custom Value (if over a hundred print guaranteed)
+        self.critchance = crit + 50 #For Display in Combat only | Weaponcritchance + Custom Value (if over a hundred print guaranteed)
+        self.strscl = 14 # A | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.dexscl = 0 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.arcscl = 0 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.faiscl = 0 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        if run == True:
+            self.function = dmg, str, dex, arc, fai, hit, crit = uppercut(dmg, str, dex, arc, fai, self.hitchance, self.critchance, self.strscl, self.dexscl, self.arcscl, self.faiscl) #Function of said Ability
+            hp = hp - self.hpcost
+            mp = mp - self.mpcost
+            return hp, mp, dmg, str, dex, arc, fai, hit, crit
+
+    def uppercut(self, hp, mp, dmg, str, dex, arc, fai, hit, crit, run):
         self.name = "" #For Display in Combat only
         self.hpcost = 0 #For Display in Combat only | Custom Value
         self.mpcost = 3 #For Display in Combat only | Custom Value
         self.finaldmg = 0
         self.finalheal = 0
         self.hitchance = hit + -10 #For Display in Combat only | Weaponhitchance + Custom Value (if over a hundred print guaranteed)
-        self.critchance = crit + 50 #For Display in Combat only | Weaponcritchance + Custom Value (if over a hundred print guaranteed)
-        self.strscl = "" #Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2%
-        self.dexscl = "" #Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2%
-        self.arcscl = "" #Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2%
-        self.faiscl = "" #Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2%
-        #self.function = overheadsmash(hp, dmg, str, dex, arc, fai, hit, crit, self.strscl, self.dexscl, self.arcscl, self.faiscl,) #Function of said Ability
+        self.critchance = crit + 0 #For Display in Combat only | Weaponcritchance + Custom Value (if over a hundred print guaranteed)
+        self.strscl = 14 # A | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.dexscl = 14 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.arcscl = 0 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        self.faiscl = 0 # - | Scaling A = 14% | B = 8% | C = 6% | D = 4% | E = 2% | - = 0%
+        if run == True:
+            self.function = dmg, str, dex, arc, fai, hit, crit = uppercut(dmg, str, dex, arc, fai, self.hitchance, self.critchance, self.strscl, self.dexscl, self.arcscl, self.faiscl) #Function of said Ability
+            hp = hp - self.hpcost
+            mp = mp - self.mpcost
+            return hp, mp, dmg, str, dex, arc, fai, hit, crit
 
-
-
-
-
-
+#---------------SHORTSWORD---------------
 
 #<<<MACE>>>
-def overheadsmash(hp, mp, dmg, str, dex, arc, fai, hit, crit): #hp for hp cost or healing | mp for mana cost or regen | dmg for dmg calc | str,dex,arc,fai stats for scaling
-    hpcost = 0
-    mpcost = 3
+def overheadsmash(dmg, str, dex, arc, fai, hit, crit, strscl, dexscl, arcscl, faiscl): #hp for hp cost or healing | mp for mana cost or regen | dmg for dmg calc | str,dex,arc,fai stats for scaling
     basedmg = 4
     baseheal = 0
-    hitchance = -10
-    critchance = 50
-    strscl = 14 #A
-    dexscl = 0 #-
-    arcscl = 0 #-
-    faiscl = 0 #-
-    hit = hit + hitchance
-    crit = crit + critchance
-    hp = hp - hpcost
-    mp = mp - mpcost
-    dmg = round((dmg + basedmg) * (((strscl * str) / 100) + ((dexscl * dex) / 100) + ((arcscl * arc) / 100) + ((faiscl * fai) / 100)),2)
+    dmg = round((dmg + basedmg) * (1 + (((strscl * (str / 100)/10)) + ((dexscl * (dex / 100)/10)) + ((arcscl * (arc / 100)/10)) + ((faiscl * (fai / 100)/10)))),2)
     if random.randint(1,100) <= crit:
         dmg *= 2
-    return hp, mp, dmg, str, dex, arc, fai, hit, crit
+    return dmg, str, dex, arc, fai, hit, crit
 
-def uppercut(hp, mp, dmg, str, dex, arc, fai, hit, crit): #hp for hp cost or healing | mp for mana cost or regen | dmg for dmg calc | str,dex,arc,fai stats for scaling
-    hpcost = 0
-    mpcost = 3
+def uppercut(dmg, str, dex, arc, fai, hit, crit, strscl, dexscl, arcscl, faiscl): #hp for hp cost or healing | mp for mana cost or regen | dmg for dmg calc | str,dex,arc,fai stats for scaling
     basedmg = 0
     baseheal = 0
-    hitchance = -10
-    critchance = 0
-    strscl = 14 #A
-    dexscl = 14 #A
-    arcscl = 0 #-
-    faiscl = 0 #-
-    hit = hit + hitchance
-    crit = crit + critchance
-    hp = hp - hpcost
-    mp = mp - mpcost
-    dmg = round((dmg + basedmg) * (((strscl * str) / 100) + ((dexscl * dex) / 100) + ((arcscl * arc) / 100) + ((faiscl * fai) / 100)),2)
+    dmg = round((dmg + basedmg) * (1 + (((strscl * (str / 100)/10)) + ((dexscl * (dex / 100)/10)) + ((arcscl * (arc / 100)/10)) + ((faiscl * (fai / 100)/10)))),2)
     if random.randint(1,100) <= crit:
         dmg *= 2
-    return hp, mp, dmg, str, dex, arc, fai, hit, crit
+    return dmg, str, dex, arc, fai, hit, crit
 
 
 
@@ -236,26 +231,43 @@ def lowermanaregen(hp, mp, dmg, str, dex, arc, fai, hit, crit): #hp for hp cost 
     mp = mp - mpcost
     return hp, mp, dmg, str, dex, arc, fai, hit, crit
 
+#Variables
+run = False
+
 #-------------TESTING FOR CALCULATION FUNCTIONS---------------
-hp = 20
-mp = 30
-dmg = 4.59
-str = 8
-dex = 6
-arc = 22
-fai = 19
-hit = 90
-crit = 5
-print(hp, mp, dmg, str, dex, arc, fai, hit, crit)
-hp, mp, dmg, str, dex, arc, fai, hit, crit = lightflash(hp, mp, dmg, str, dex, arc, fai, hit, crit)
-print(hp, mp, dmg, str, dex, arc, fai, hit, crit)
+# hp = 20
+# mp = 30
+# dmg = 13.92
+# str = 24
+# dex = 18
+# arc = 22
+# fai = 19
+# hit = 90
+# crit = 5
+# print(hp, mp, dmg, str, dex, arc, fai, hit, crit)
+# hp, mp, dmg, str, dex, arc, fai, hit, crit = lightflash(hp, mp, dmg, str, dex, arc, fai, hit, crit)
+# print(hp, mp, dmg, str, dex, arc, fai, hit, crit)
 
 #-------------TESTING FOR REWORK---------------
 #abili = abilities[1]
-abi = abilities()
-abimethods = [target for target, method in inspect.getmembers(abilities, predicate=inspect.isfunction)     
-    if not target.startswith("_")]
-#abiindex = abimethods.index(abili)
-currentabi = abimethods[0]
-getattr(abi, currentabi)(hp, mp, dmg, str, dex, arc, fai, hit, crit)
-print(abi.hitchance)
+# run = False
+# abi = abilities()
+# abimethods = [target for target, method in inspect.getmembers(abilities, predicate=inspect.isfunction)     
+#     if not target.startswith("_")]
+# #abiindex = abimethods.index(abili)
+# currentabi = abimethods[0]
+# getattr(abi, currentabi)(hp, mp, dmg, str, dex, arc, fai, hit, crit, run)
+# loop = True
+# while loop == True:
+#     print(dmg)
+#     print(f"Costs: HP = {abi.hpcost} | MP = {abi.mpcost}")
+#     print(f"Scaling: STR = {abi.strscl}% | DEX = {abi.dexscl}%")
+#     print(f"         ARC = {abi.arcscl}% | FAI = {abi.faiscl}%")
+#     desc = input("Ja/Nein: ").lower().replace(" ","")
+#     if desc == "ja":
+#         run = True
+#         hp, mp, dmg, str, dex, arc, fai, hit, crit = getattr(abi, currentabi)(hp, mp, dmg, str, dex, arc, fai, hit, crit, run)
+#         print(f"You dealt {dmg}")
+#         loop = False
+#     else:
+#         print("Bist zu dumm?")
